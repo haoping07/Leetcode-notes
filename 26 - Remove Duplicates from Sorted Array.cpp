0b673d9f04e39720697b3c30c0c99dfcfb907251
Hao-Ping Lin <pinglin927@gmail.com>
2019-10-01 19:01:39 -0400

    Fixed indent

--- 26 - Remove Duplicates from Sorted Array.cpp
+++ 26 - Remove Duplicates from Sorted Array.cpp
@@ -5,18 +5,18 @@
 
 class Solution {
 public:
-	int removeDuplicates(vector<int>& nums) {
-		if (nums.empty())
-			return 0;
-		int start = 0;
-		for (int i = 1; i < nums.size(); i++)
-		{
-			if (nums[start] != nums[i])
-			{
-				start++;
-				nums[start] = nums[i];
-			}
-		}
-		return start + 1;
-	}
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+        int start = 0;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[start] != nums[i])
+            {
+                start++;
+                nums[start] = nums[i];
+            }
+        }
+        return start + 1;
+    }
 };
--- 27 - Remove Element.cpp
+++ 27 - Remove Element.cpp
@@ -5,16 +5,16 @@
 
 class Solution {
 public:
-	int removeElement(vector<int>& nums, int val) {
-		int p = 0;
-		for (int i = 0; i < nums.size(); i++)
-		{
-			if (nums[i] != val)
-			{
-				nums[p] = nums[i];
-				p++;
-			}
-		}
-		return p;
-	}
+    int removeElement(vector<int>& nums, int val) {
+        int p = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[p] = nums[i];
+                p++;
+            }
+        }
+        return p;
+    }
 };
