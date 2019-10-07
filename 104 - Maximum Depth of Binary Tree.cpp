817adcdbdad5ff1eef087db90f2d071c0a4468b8
Hao-Ping Lin <pinglin927@gmail.com>
2019-10-07 02:30:23 -0400

    Initial commit

---
+++ 104 - Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,44 @@
+/*
+ 104. Maximum Depth of Binary Tree
+ Easy
+ How: Recursive, BFS
+*/
+
+// Recursive
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root)
+            return 0;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return left > right ? left + 1 : right + 1;
+    }
+};
+
+// BFS
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root)
+            return 0;
+        queue<TreeNode*> todo;
+        todo.push(root);
+        int count = 0;
+        while (!todo.empty())
+        {
+            int size = todo.size();
+            count++;
+            for (int i = 0; i < size; i++)
+            {
+                root = todo.front();
+                todo.pop();
+                if (root->left)
+                    todo.push(root->left);
+                if (root->right)
+                    todo.push(root->right);
+            }
+        }
+        return count;
+    }
+};
