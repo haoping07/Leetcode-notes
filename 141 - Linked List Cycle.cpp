aaff116154ef8cd5c28e7b122a1632244e4f6459
Hao-Ping Lin <pinglin927@gmail.com>
2019-10-12 22:54:12 -0400

    Initial commit

---
+++ 141 - Linked List Cycle.cpp
@@ -0,0 +1,27 @@
+/*
+ 141. Linked List Cycle (Easy)
+
+ How:
+    fast-slow pointer
+    1. Make slow pointer points to node
+    2. Make fast pointer points to node->next
+    When inside the cycle, because fast pointer walks two node at each iterate, it approaches slow pointer step by step.
+    If fast pointer catches up slow pointer, it means this linked list have cycle.
+*/
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if (!head)
+            return false;
+        ListNode* slow = head, * fast = head->next;
+        while (fast != nullptr && fast->next != nullptr)
+        {
+            if (slow == fast)
+                return true;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return false;
+    }
+};
