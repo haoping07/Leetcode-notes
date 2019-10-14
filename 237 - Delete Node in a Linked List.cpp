ea744e28d400a48e636d7ca014746ef41fa0e42f
Hao-Ping Lin <pinglin927@gmail.com>
2019-10-14 17:28:42 -0400

    Initial commit

---
+++ 237 - Delete Node in a Linked List.cpp
@@ -0,0 +1,23 @@
+/*
+ 237. Delete Node in a Linked List (Easy)
+
+ How:
+    Brute force - O(n)
+    Only the node that needs to be deleted is given, we replace this node by its next node connect to its next next node.
+    Pay attention to the corner cases: 1. Node doesn't exist 2. Last node
+*/
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if (!node)
+            return;
+        if (!node->next)
+        {
+            node = nullptr;
+            return;
+        }
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
