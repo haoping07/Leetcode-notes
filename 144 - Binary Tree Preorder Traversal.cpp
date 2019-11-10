1f028d3021b554b2ddbf66dc04f09f0abf5198a2
Hao-Ping Lin <pinglin927@gmail.com>
2019-11-10 18:26:29 -0500

    Initial commit

---
+++ 144 - Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,60 @@
+/*
+ 144. Binary Tree Preorder Traversal (Medium)
+
+ Notes:
+    Tree trasverse - BFS or DFS. Use DFS here o(n)
+    Approach 1: Trasverse left child till the end, then check the right child
+    Approach 2: In each iteration, push current node's val to result, then check whether if 
+    left / right child exist. If yes, push the right child then left child into the stack
+*/
+
+// Approach 1
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> todo;
+        vector<int> res;
+
+        while (!todo.empty() || root)
+        {
+            if (root)
+            {
+                res.push_back(root->val);
+                todo.push(root);
+                root = root->left;
+            }
+            else
+            {
+                root = todo.top();
+                todo.pop();
+                root = root->right;
+            }
+        }
+        return res;
+    }
+};
+
+// Approach 2
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if (!root) return {};
+        vector<int> res;
+        stack<TreeNode*> todo;
+        todo.push(root);
+
+        while (!todo.empty())
+        {
+            root = todo.top();
+            todo.pop();
+            res.push_back(root->val);
+
+            if (root->right)
+                todo.push(root->right);
+            if (root->left)
+                todo.push(root->left);
+        }
+
+        return res;
+    }
+};
