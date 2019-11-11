c51cc372f2d8a8279838ba87952071e7d7851d65
Hao-Ping Lin <pinglin927@gmail.com>
2019-11-11 18:01:58 -0500

    Initial commit

---
+++ 101 - Symmetric Tree.cpp
@@ -0,0 +1,56 @@
+/*
+ 101. Symmetric Tree (Easy)
+
+*/
+
+// BFS
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        TreeNode* left = nullptr, * right = nullptr;
+        queue<TreeNode*> todo;
+        todo.push(root->left);
+        todo.push(root->right);
+        while (!todo.empty())
+        {
+            left = todo.front();
+            todo.pop();
+            right = todo.front();
+            todo.pop();
+
+            if (left == nullptr && right == nullptr)
+                continue;
+            if (left == nullptr || right == nullptr)
+                return false;
+            if (left->val != right->val)
+                return false;
+
+            todo.push(left->left);
+            todo.push(right->right);
+            todo.push(left->right);
+            todo.push(right->left);
+        }
+        return true;
+    }
+};
+
+// Recursive
+class Solution {
+public:
+    bool CheckMirror(TreeNode* left, TreeNode* right)
+    {
+        if (left == nullptr && right == nullptr)
+            return true;
+        if (left == nullptr || right == nullptr)
+            return false;
+        if (left->val != right->val)
+            return false;
+        return CheckMirror(left->left, right->right) && CheckMirror(left->right, right->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        return CheckMirror(root->left, root->right);
+    }
+};
