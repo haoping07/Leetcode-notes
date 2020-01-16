877aafbdfe32777a490dcb37a8fd988eb1285cb4
Hao-Ping Lin <pinglin927@gmail.com>
2020-01-16 18:42:28 -0500

    Add Divide and Conquer approach

--- 98 - Validate Binary Search Tree.cpp
+++ 98 - Validate Binary Search Tree.cpp
@@ -1,15 +1,28 @@
 /*
- 98. Validate Binary Search Tree
- Medium
+ 98. Validate Binary Search Tree (M)
+ 
+ Notes:
+    Approach 1 : Inorder characteristic
+    Inorder traversal result is sorted.
+      T: O(2n)    // n is node number
+      S: O(n)    // n is node number
+    
+    Approach 2 : Divide and Conquer
+    Record each node as its child node's Maximum / Minimum edge.
+    Use [Node] to record the max / min edge because sometimes the givien node's value
+    is excceed INT_MAX / INT_MIN
+      T: O(n)    // n is node number
+      S: O(n)    // n is node number
 */
 
+// Approach 1: Inorder characteristic
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
         if (!root) return true;
         inorder(root);
         int size = listNode.size();
-        for (int i = 0; i < size - 1; i++)
+        for (int i = 0; i < size - 1; i++) 
         {
             if (listNode[i] >= listNode[i + 1])
                 return false;
@@ -26,4 +39,23 @@ public:
     }
 private:
     vector<int> listNode;
+};
+
+// Approach 2: Divide and Conquer
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return VBST(root, nullptr, nullptr);
+    }
+
+    bool VBST(TreeNode* root, TreeNode* minNode, TreeNode* maxNode)
+    {
+        if (!root) return true;
+        if (minNode != nullptr && root->val <= minNode->val)
+            return false;
+        if (maxNode != nullptr && root->val >= maxNode->val)
+            return false;
+        return VBST(root->left, minNode, root) &&
+            VBST(root->right, root, maxNode);
+    }
 };
