9dba7f5c840c4bb6da14457cae0deb6d9620e418
Hao-Ping Lin <pinglin927@gmail.com>
2020-01-16 17:30:29 -0500

    Update notes

--- 103 - Binary Tree Zigzag Level Order Traversal.cpp
+++ 103 - Binary Tree Zigzag Level Order Traversal.cpp
@@ -1,36 +1,40 @@
 /*
- 103. Binary Tree Zigzag Level Order Traversal
- Medium
+ 103. Binary Tree Zigzag Level Order Traversal (M)
+ 
+ Notes:
+    T: O(nodes + 2 * depth / 2)     // (2 * depth / 2): Each level at most two nodes, 
+                                    // only even level (half of the depth) needs reverse 
+    S: O(n)
+    Use a count to track if the current level is even or odd. If even, reverse the vector in that level.
 */
 
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         if (!root) return {};
-        vector<vector<int>> result;
         queue<TreeNode*> todo;
         todo.push(root);
-        int curLevel = 0;
+        vector<vector<int>> res;
+        int level = 0;
         while (!todo.empty())
         {
             int size = todo.size();
             vector<int> tmp;
+            level++;
             for (int i = 0; i < size; i++)
             {
                 root = todo.front();
                 todo.pop();
+                tmp.push_back(root->val);
                 if (root->left)
                     todo.push(root->left);
                 if (root->right)
                     todo.push(root->right);
-                tmp.push_back(root->val);
             }
-
-            if (curLevel % 2 == 1)
+            if (level % 2 == 0)
                 reverse(tmp.begin(), tmp.end());
-            result.push_back(tmp);
-            curLevel++;
+            res.push_back(tmp);
         }
-        return result;
+        return res;
     }
 };
