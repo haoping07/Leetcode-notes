ba2ffb5eea9a925dcb65fdd0e7472548431e793a
Hao-Ping Lin <pinglin927@gmail.com>
2020-01-17 18:31:47 -0500

    Update notes

--- 235 - Lowest Common Ancestor of a Binary Search Tree.cpp
+++ 235 - Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -1,19 +1,28 @@
 /*
- 235. Lowest Common Ancestor of a Binary Search Tree
- Easy
+ 235. Lowest Common Ancestor of a Binary Search Tree (E)
+ 
+ Notes:
+    Approach 1 :
+    If two nodes' value is bigger than root, then the LCA must be in right subtree,
+    else if smaller than root, then must in left subtree.
+    Else, root is the LCA.
+      T: O(log(n))
+      S: O(log(n))
+    
 */
 
+// Approach 1 :
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        int rval = root->val;
-        int pval = p->val;
-        int qval = q->val;
+        int pv = p->val;
+        int qv = q->val;
+        int rv = root->val;
 
-        if (pval > rval&& qval > rval)
-            return lowestCommonAncestor(root->right, p, q);
-        else if (pval < rval && qval < rval)
+        if (pv < rv && qv < rv)
             return lowestCommonAncestor(root->left, p, q);
+        else if (pv > rv && qv > rv)
+            return lowestCommonAncestor(root->right, p, q);
         else
             return root;
     }
