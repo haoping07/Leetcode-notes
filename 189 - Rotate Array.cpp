33a00417ce8252514375c154245f6e74b10992b0
Hao-Ping Lin <pinglin927@gmail.com>
2020-02-02 01:12:05 -0500

    Add approach

--- 189 - Rotate Array.cpp
+++ 189 - Rotate Array.cpp
@@ -3,9 +3,46 @@
  Easy
 
  Notes: 
+ A1:
+ Roate one by one
+   T: O(n)
+   S: O(1)
+
+ A2:
  Rotate three time
+   T: O(n)
+   S: O(1)
 */
 
+// Approach 1
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if (nums.size() < 2 || k == 0) return;
+        k = k % nums.size();
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int startIdx = i, startVal = nums[i];
+            do
+            {
+                int rightPos = (startIdx + k) % nums.size();
+                int tmp = nums[rightPos];
+                nums[rightPos] = startVal;
+
+                startIdx = rightPos;
+                startVal = tmp;
+
+                count++;
+            } while (startIdx != i);
+
+            if (count == nums.size()) break;
+        }
+    }
+};
+
+// Approach 2
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
@@ -15,3 +52,4 @@ public:
         reverse(nums.begin(), nums.end());
     }
 };
+
