13f744b1b966356d0681513b9a65f5484a9fcc95
Hao-Ping Lin <pinglin927@gmail.com>
2020-02-09 01:48:21 -0500

    Update notes

--- 299 - Bulls and Cows.cpp
+++ 299 - Bulls and Cows.cpp
@@ -1,26 +1,34 @@
 /*
- 299. Bulls and Cows
- Easy
+ 299. Bulls and Cows (E)
+
+ Notes:
+    Use hashmap (array) and two For-loop.
+    First loop (Count bull): iterate each letter in string secret and guess. 
+    If two letters are same, bull++. Count the appear of letter and store in the hashmap
+    Second loop (Count cow): iterate two letter-count array and take the minumum in each. 
+    The total amount of the count - bull is cow
+    T: O(n)
+    S: O(1)
+
 */
 
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        int a = 0, b = 0;
-        int arr1[10] = { 0 }, arr2[10] = { 0 };
+        int bull = 0, cow = 0;
+        int cs[10] = { 0 }, cg[10] = { 0 };
         for (int i = 0; i < secret.size(); i++)
         {
             int x = secret[i] - '0';
             int y = guess[i] - '0';
-            if (x == y)
-                a++;
-            arr1[x]++;
-            arr2[y]++;
+            if (x == y) bull++;
+            cs[x]++;
+            cg[y]++;
         }
 
         for (int i = 0; i < 10; i++)
-            b += min(arr1[i], arr2[i]);
+            cow += min(cs[i], cg[i]);
 
-        return to_string(a) + "A" + to_string(b - a) + "B";
+        return to_string(bull) + "A" + to_string(cow - bull) + "B";
     }
 };
