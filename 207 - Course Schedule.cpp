b93f7c87becdefd43f976d805a152a98ebbc10ef
Hao-Ping Lin <pinglin927@gmail.com>
2020-02-19 17:09:25 -0500

    Update notes and code

--- 207 - Course Schedule.cpp
+++ 207 - Course Schedule.cpp
@@ -1,59 +1,65 @@
 /*
  207. Course Schedule (M)
- 
+
  Notes:
-    It is possible to finish all courses if 'cycle' exist.
+    If cycle exist in graph, it is impossible to finish all courses.
     Approach 1 : DFS
-     1. Build graph vector
-     2. Create visited node vector
-     3. Iterate all nodes
-        0. If the node status is 1, cycle exist. Return fail
-           If the node status is 2, cycle not exist from that node. Return ok
-        1. Set status to 1
-        2. Iterate its neighbors by DFS
-        3. If all neighbors checked, set stauts to 2
-        4. Return ok
-    T: O(n)
-    S: O(n)
+    1. Build graph
+    2. Create status map and init to 0 for each node (0: unseen, 1: in DFS process, 2: finished DFS process)
+    3. DFS each nodes
+      3.1. Check status. If 1, return true (have cycle). If 2, return true (visited, ok node)
+      3.2. If unseen, set status to 1 and DFS its neighbors
+    4. If no cycle, all courses can be finished
+    T: O(n), traverse all nodes
+    S: O(n), status map
+
 */
 
+// *Approach 1 : DFS
 class Solution {
+private:
+    vector<vector<int>> graph;
+    vector<int> status;
+
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        graph = vector<vector<int>>(numCourses);
-        vector<int> status(numCourses, 0);
-        for (const auto& p : prerequisites)
+        graph = vector<vector<int>>(numCourses, vector<int>());
+        status = vector<int>(numCourses, 0);
+
+        // Build graph
+        for (const vector<int>& p : prerequisites)
             graph[p[0]].push_back(p[1]);
- 
+
+        // BFS the graph (to check if cycle exist)
         for (int i = 0; i < numCourses; i++)
         {
-            if (DFS(i, status))
+            if (DFS(i))
                 return false;
         }
         return true;
     }
- 
-    bool DFS(int cur, vector<int>& status)
+
+    bool DFS(int cur)
     {
+        // If cur is still in DFS process
         if (status[cur] == 1)
             return true;
 
+        // If cur was finished DFS process beforehand
         if (status[cur] == 2)
             return false;
- 
+
+        // Set status to 1 (In DFS process) and start DFS
         status[cur] = 1;
- 
-        for (const auto& neighbor : graph[cur])
+
+        for (const int& neighbor : graph[cur])
         {
-            if (DFS(neighbor, status))
+            if (DFS(neighbor))
                 return true;
         }
 
+        // Set status to 2 (Finish DFS process)
         status[cur] = 2;
         return false;
-    }        
- 
- 
-private:
-    vector<vector<int>> graph;
+    }
 };
