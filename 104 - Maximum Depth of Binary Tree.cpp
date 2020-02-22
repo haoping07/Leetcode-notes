23ff92595d289e0ce353379ad52b063402b90603
Hao-Ping Lin <pinglin927@gmail.com>
2020-02-22 18:20:29 -0500

    Add new DFS approach

--- 104 - Maximum Depth of Binary Tree.cpp
+++ 104 - Maximum Depth of Binary Tree.cpp
@@ -1,45 +1,24 @@
 /*
- 104. Maximum Depth of Binary Tree (Easy)
+ 104. Maximum Depth of Binary Tree (E)
  
- Notes: Recursive, BFS
+ Notes:
+ Approach 1 : BFS
+ Time: O(n)
+ Space: O(n)
+
+ Approach 2 : DFS
+ Time: O(n)
+ Space: O(n)
+
 */
 
-// Recursive 1
-class Solution {
-public:
-    int maxDepth(TreeNode* root) {
-        if (!root) return 0;
-        int left = maxDepth(root->left);
-        int right = maxDepth(root->right);
-        return left > right ? left + 1 : right + 1;
-    }
-};
-
-// Recursive 2 (slower)
-class Solution {
-public:
-    void DFS(TreeNode* root, int depth, int& res)
-    {
-        if (!root) return;
-        res = max(res, depth);
-        DFS(root->left, depth + 1, res);
-        DFS(root->right, depth + 1, res);
-    }
-
-    int maxDepth(TreeNode* root) {
-        int res = 0;
-        DFS(root, 1, res);
-        return res;
-    }
-};
-
-
-// BFS
+// Approahc 1 : BFS
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if (!root)
             return 0;
+
         queue<TreeNode*> todo;
         todo.push(root);
         int count = 0;
@@ -60,3 +39,27 @@ public:
         return count;
     }
 };
+
+
+// Approahc 2 : Recursive 1
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return left > right ? left + 1 : right + 1;
+    }
+};
+
+// Approahc 2 : Recursive 2
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root)
+            return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+        return 0;
+    }
+};
+
+
