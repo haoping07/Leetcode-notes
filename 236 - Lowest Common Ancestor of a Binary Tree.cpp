972dfcfba53062587b2c28c53200119e5d0a0048
Hao-Ping Lin <pinglin927@gmail.com>
2020-02-22 01:53:59 -0500

    Init commit

---
+++ 236 - Lowest Common Ancestor of a Binary Tree.cpp
@@ -0,0 +1,31 @@
+
+/*
+ 236. Lowest Common Ancestor of a Binary Tree (M)
+
+ Notes:
+   Approach 1 : Postorder traversal
+   Use Postorder traversal. If found, return to parent. 
+   Time: O(n), traverse all nodes
+   Space: O(n)
+
+*/
+
+// Approach 1 : Postorder traversal
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root)
+            return nullptr;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (root == p || root == q)
+            return root;
+
+        if (left && right)
+            return root;
+
+        return left == nullptr ? right : left;
+    }
+};
