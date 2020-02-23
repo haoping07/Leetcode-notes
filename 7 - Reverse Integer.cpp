63ef4f198f58659f657d4f3ac72f9d15a9c0be58
Hao-Ping Lin <pinglin927@gmail.com>
2020-02-23 16:37:36 -0500

    Add approach

--- 7 - Reverse Integer.cpp
+++ 7 - Reverse Integer.cpp
@@ -25,3 +25,23 @@ public:
         return rev;
     }
 };
+
+
+// Approach 1 : 
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while (x != 0)
+        {
+            int mod = x % 10;
+            if (res > INT_MAX / 10 || (res == INT_MAX / 10 && mod > INT_MAX % 10))
+                return 0;
+            if (res < INT_MIN / 10 || (res == INT_MIN / 10 && mod < INT_MIN % 10))
+                return 0;
+            res = res * 10 + mod;
+            x /= 10;
+        }
+        return res;
+    }
+};
