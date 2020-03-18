bd39a36053363a2ffad698fc4d6a2a2d44d447c4
Hao-Ping Lin <pinglin927@gmail.com>
2020-03-18 01:48:59 -0400

    Update notes

--- 94 - Binary Tree Inorder Traversal.cpp
+++ 94 - Binary Tree Inorder Traversal.cpp
@@ -1,22 +1,17 @@
 /*
  94. Binary Tree Inorder Traversal (M)
  
- Notes:
- Approach 1 :
- Recursion
+ Approach 1 : Recursion
  Time: O(n)
  Space: O(n)
 
- Approach 2 :
- Iteration
+ Approach 2 : Iteration
  Time: O(n)
  Space: O(n)
 
- * Ref 144: The Approach 2 in Q.144 can be used here, but it will need to track the visited node to know when
-            to push the node value to the result vector.
 */
 
-// Approach 1 : Recursion
+/* Approach 1 : Recursion */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
@@ -36,7 +31,7 @@ private:
     vector<int> res;
 };
 
-// Approach 2 : Iteration
+/* Approach 2 : Iteration */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
