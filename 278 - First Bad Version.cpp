9c186a3b83179eb855c4af8970f577f8ddfb8aa7
Hao-Ping Lin <pinglin927@gmail.com>
2020-03-23 21:14:29 -0400

    Update notes

--- 278 - First Bad Version.cpp
+++ 278 - First Bad Version.cpp
@@ -1,18 +1,24 @@
 /*
- 278. First Bad Version
- Easy
+ 278. First Bad Version (E)
+ 
+ Approach 1 : Binary Search
+ Time: O(logn)
+ Space: O(1)
+
 */
 
+/* Approach 1 : Binary Search */
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int l = 1, r = n;
+        int l = 1, r = n, m = 0;
         while (l < r)
         {
-            int m = l + (r - l) / 2;
+            m = l + (r - l) / 2;
             if (isBadVersion(m))
+                // Because m is always the left one if the list size is even
                 r = m;
-            else
+            else 
                 l = m + 1;
         }
         return l;
