b5c9a8db73bac36bf7c4ad56127174dfcb9fdcdf
Hao-Ping Lin <pinglin927@gmail.com>
2020-03-24 18:44:02 -0400

    Init commit

---
+++ JAVA/206 - Reverse Linked List.java
@@ -0,0 +1,39 @@
+/*
+ 206. Reverse Linked List (E)
+
+ Approach 1 : Iteration
+ Time: O(n)
+ Space: O(1)
+
+Approach 2 : Recursion
+ Time: O(n)
+ Space: O(n)
+
+*/
+
+/* Approach 1 */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode next = null;
+        while (head != null)
+        {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
+
+/* Approach 2 */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
