7f741bf3981e194e25db2e66550cdb051200c5a8
Hao-Ping Lin <pinglin927@gmail.com>
2020-03-25 23:32:08 -0400

    Init commit

---
+++ JAVA/2 - Add Two Numbers.java
@@ -0,0 +1,42 @@
+/*
+ 2. Add Two Numbers (M)
+
+ Approach 1 :
+ Do the addition process one by one and save the result to variable 'carry' in 
+ each addition. carry % 10 is the value of new node, carry / 10 is the new carry
+ that will be added to the next digit
+ Time: O(max(l1, l2))
+ Space: O(max(l1, l2))
+
+*/
+
+/* Approach 1 */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode ptr = head;
+        int carry = 0;
+        while (true) {
+            if (l1 != null) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
+            
+            ptr.val = carry % 10;
+            carry /= 10;
+            
+            if (l1 != null || l2 != null || carry != 0) {
+                ptr.next = new ListNode(0);
+                ptr = ptr.next;
+            }
+            else break;
+        }
+        return head;
+    }
+}
+
+
