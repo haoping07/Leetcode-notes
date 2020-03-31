d5a35a71d4e397046b3d52eb68e01547bcce5c05
Hao-Ping Lin <pinglin927@gmail.com>
2020-03-30 20:58:31 -0400

    Add approach

--- C++/274 - H-Index.cpp
+++ C++/274 - H-Index.cpp
@@ -1,8 +1,34 @@
 /*
- 274. H-Index
- Medium
+ 274. H-Index (M)
+ 
+ Approach 1 :
+ Time: O(n)
+ Space: O(1)
+
+ Approach 2 :
+ Time: O(n)
+ Space: O(1)
+
 */
 
+/* Approach 1 */
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        if (citations.size() == 0) return 0;
+        int size = citations.size();
+        int c = 0;
+        sort(citations.begin(), citations.end());
+        for (int i = size - 1; i >= 0; i--) {
+            if (citations[i] >= c + 1) c++;
+            else break;
+        }
+        return c;
+    }
+};
+
+
+/* Approach 2 */
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
