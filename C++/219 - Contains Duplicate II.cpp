743aebf8f45b680907aa5cd19736311c4612b9de
Hao-Ping Lin <pinglin927@gmail.com>
2020-04-04 19:40:00 -0400

    Update notes

--- C++/219 - Contains Duplicate II.cpp
+++ C++/219 - Contains Duplicate II.cpp
@@ -1,18 +1,18 @@
 /*
- 219. Contains Duplicate II
- Easy
+ 219. Contains Duplicate II (E)
 
- Notes:
-	- O(n)
-	Find duplicate in a list -> hashtable
+ Approach 1 :
+ Time: O(n)
+ Space: O(n)
+
 */
 
+/* Approach 1 */
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int, int> hash;
-        for (int i = 0; i < nums.size(); i++)
-        {
+        for (int i = 0; i < nums.size(); i++) {
             if (hash.count(nums[i]) && i - hash[nums[i]] <= k)
                 return true;
             hash[nums[i]] = i;
