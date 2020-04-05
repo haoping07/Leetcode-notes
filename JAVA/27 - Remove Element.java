785237ef90d90e12ec2ea0d550fefcc6dd2e2538
Hao-Ping Lin <pinglin927@gmail.com>
2020-04-05 02:08:36 -0400

    Init commit

---
+++ JAVA/27 - Remove Element.java
@@ -0,0 +1,19 @@
+/*
+ 27. Remove Element (E)
+ 
+ Approach 1 :
+ Time: O(n)
+ Space: O(1)
+
+*/
+
+/* Approach 1 */
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) nums[slow++] = nums[i];
+        }
+        return slow;
+    }
+}
