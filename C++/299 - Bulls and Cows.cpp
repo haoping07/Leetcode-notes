196ad4bb9f2addeb1d1d361d1638e7b286ba75eb
Hao-Ping Lin <pinglin927@gmail.com>
2020-04-06 23:12:03 -0400

    Add approach

--- C++/299 - Bulls and Cows.cpp
+++ C++/299 - Bulls and Cows.cpp
@@ -7,11 +7,17 @@
     Counts the appear of letter and stores to the array by its ASCII as key. 
  2. Iterates two arrays at the same time, take the minumum in each and adds to the cow;
  * The amount of cow is all exist numbers (whether the position is wrong) - position is correct (bull)
- Time: O(n)
- Space: O(1)
+ Time: O(secret length + 10)
+ Space: O(secret length + guess length)
+
+ Approach 2 :
+ Optimize Approach 1 to one array and one pass.
+ Time: O(secret length)
+ Space: O(count length)
 
 */
 
+/* Approach 1 : */
 class Solution {
 public:
     string getHint(string secret, string guess) {
@@ -21,13 +27,36 @@ public:
             int x = secret[i] - '0';
             int y = guess[i] - '0';
             if (x == y) bull++;
-            cs[x]++;
-            cg[y]++;
+            s[x]++;
+            g[y]++;
         }
 
         for (int i = 0; i < 10; i++)
             cow += min(s[i], g[i]);
 
         return to_string(bull) + "A" + to_string(cow - bull) + "B";
+    }
+};
+
+
+/* Approach 2 : */
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int c[10] = { 0 };
+        int bull = 0, cow = 0;
+        for (int i = 0; i < secret.size(); i++) {
+            if (secret[i] == guess[i]) bull++;
+            else {
+                int d1 = secret[i] - '0';
+                if (c[d1] < 0) cow++;
+                c[d1]++;
+
+                int d2 = guess[i] - '0';
+                if (c[d2] > 0) cow++;
+                c[d2]--;
+            }
+        }
+        return to_string(bull) + "A" + to_string(cow) + "B";
     }
 };
