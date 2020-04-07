b21a8640034cc1060b520d3999b18ea72cb79337
Hao-Ping Lin <pinglin927@gmail.com>
2020-04-07 03:54:48 -0400

    add dp approach

--- C++/55 - Jump Game.cpp
+++ C++/55 - Jump Game.cpp
@@ -1,24 +1,44 @@
 /*
- 55. Jump Game (Medium)
+ 55. Jump Game (M)
 
- Notes:
-    Greedy algorithm - O(n)
-    Scan the list from end to begin. If the list[i] value is "0", the list[i - 1] will need to have addtional step
-    to skip this element. If reach the first element and still need steps to pay off additional steps, then it is 
-    impossible to reach the end.
+ Approach 1 : Backtracking   
+ Scan the list from end to head. If the current scanned element is 0, it means
+ the next scanned element will need to have one additional step to pass this 
+ element, or it will stuck in here. If scan is over but still need steps to pay
+ off, then it is impossible to reach the end.
+ Time: O(n)
+ Space: O(1)
+
+ Approach 2 : DP
+ Time: O(n)
+ Space: O(n)
+
 */
 
+/* Approach 1 */
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int gap = 0;
-        for (int i = nums.size() - 2; i >= 0; i--)
-        {
-            if (nums[i] - gap <= 0)
-                gap++;
+        int step = 0;
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            if (nums[i] - step > 0)
+                step = 0;
             else
-                gap = 0;
+                step++;
         }
-        return gap == 0;
+        return step == 0;
+    }
+};
+
+/* Approach 2 */
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        vector<int> dp(nums.size(), 0);
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 1], nums[i - 1]) - 1;
+            if (dp[i] < 0) return false;
+        }
+        return true;
     }
 };
