a8dbf1e91dc29c1350701ca120ab739989d9bc26
Hao-Ping Lin <pinglin927@gmail.com>
2020-04-06 23:31:31 -0400

    Add approach

--- C++/118 - Pascal's Triangle.cpp
+++ C++/118 - Pascal's Triangle.cpp
@@ -1,7 +1,11 @@
 /*
  118. Pascal's Triangle (E)
 
- Notes:
+ Approach 1 :
+ Time: O(n)
+ Space: O(n)
+
+ Approach 2 : Brute force
  Time: O(n)
  Space: O(n)
 
@@ -17,6 +21,31 @@ public:
             for (int j = 1; j < i; j++) {
                 res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
             }
+        }
+        return res;
+    }
+};
+
+/* Approach 2 */
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        if (numRows == 0) return {};
+        vector<vector<int>> res(numRows, vector<int>());
+
+        // First layer
+        res[0].push_back(1);
+
+        for (int i = 1; i < numRows; i++) {
+            // First element always 1
+            res[i].push_back(1);
+
+            for (int j = 1; j < i; j++) {
+                res[i].push_back(res[i - 1][j - 1] + res[i - 1][j]);
+            }
+
+            // Last element always 1
+            res[i].push_back(1);
         }
         return res;
     }
