2d13af1fa58587e5543f36c3358d7c3510613bce
Hao-Ping Lin <pinglin927@gmail.com>
2020-04-09 18:21:00 -0400

    Update code

--- JAVA/118 - Pascal's Triangle.java
+++ JAVA/118 - Pascal's Triangle.java
@@ -10,25 +10,29 @@
 /* Approach 1 */
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (numRows == 0) return res;
         
-        if (numRows == 0) return triangle;
+        // First layer always 1
+        res.add(new ArrayList<Integer>());
+        res.get(0).add(1);
         
-        // First layer
-        triangle.add(new ArrayList<Integer>());
-        triangle.get(0).add(1);
-        
-        // Following layers
         for (int i = 1; i < numRows; i++) {
             List<Integer> row = new ArrayList<Integer>();
-            List<Integer> prevRow = triangle.get(i - 1);
+            List<Integer> prevRow = res.get(i - 1);
+            
+            // First element
             row.add(1);
+            
             for (int j = 1; j < i; j++) {
                 row.add(prevRow.get(j - 1) + prevRow.get(j));
             }
+            
+            // Last element
             row.add(1);
-            triangle.add(row);
+            // Add row to list
+            res.add(row);
         }
-        return triangle;
+        return res;
     }
 }
