302bad8bd0910ec3157dc2a557e33a615e0a1b9a
Hao-Ping Lin <pinglin927@gmail.com>
2020-04-17 19:33:15 -0400

    Update code

--- C++/299 - Bulls and Cows.cpp
+++ C++/299 - Bulls and Cows.cpp
@@ -6,7 +6,6 @@
  1. Iterates secret and guess at the same time. If two letter are the same, bull++.
     Counts the appear of letter and stores to the array by its ASCII as key. 
  2. Iterates two arrays at the same time, take the minumum in each and adds to the cow;
- * The amount of cow is all exist numbers (whether the position is wrong) - position is correct (bull)
  Time: O(secret length + 10)
  Space: O(secret length + guess length)
 
@@ -27,14 +26,16 @@ public:
             int x = secret[i] - '0';
             int y = guess[i] - '0';
             if (x == y) bull++;
-            s[x]++;
-            g[y]++;
+            else {
+                s[x]++;
+                g[y]++;
+            }
         }
 
         for (int i = 0; i < 10; i++)
             cow += min(s[i], g[i]);
 
-        return to_string(bull) + "A" + to_string(cow - bull) + "B";
+        return to_string(bull) + "A" + to_string(cow) + "B";
     }
 };
 
