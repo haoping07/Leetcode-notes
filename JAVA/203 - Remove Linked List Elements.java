d91a63694b5f41aba2d41c4a460d7d5100d04076
Hao-Ping Lin <pinglin927@gmail.com>
2020-05-02 01:02:10 -0400

    Update notes

--- JAVA/203 - Remove Linked List Elements.java
+++ JAVA/203 - Remove Linked List Elements.java
@@ -1,7 +1,8 @@
 /*
  203. Remove Linked List Elements (E)
 
- Approach 1 :
+ Approach 1 : One pointer
+ Check if node's next exist, if exist, compare two nodes val and relink. 
  Time: O(n)
  Space: O(1)
 
@@ -13,7 +14,7 @@ class Solution {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode node = dummy;
-        while (node != null && node.next != null) {
+        while (node.next != null) {
             if (node.next.val == val)
                 node.next = node.next.next;
             else node = node.next;
