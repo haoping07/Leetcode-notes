0f8a3187dda03a9c053efe83be9d15d40781abea
Hao-Ping Lin <pinglin927@gmail.com>
2020-05-05 22:16:10 -0400

    Init commit

---
+++ JAVA/35 - Search Insert Position.java
@@ -0,0 +1,28 @@
+/*
+ 35. Search Insert Position (E)
+ 
+ Approach 1 : Binary Search
+ Time: O(logn)
+ Space: O(1)
+
+ Think: How do I know when to mid - 1 or mid + 1
+ Try the two nodes case and decide based on the problem.
+ 
+*/
+
+/* Approach 1 */
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (target == nums[mid]) 
+                return mid;
+            else if (target < nums[mid])
+                high = mid;
+            else
+                low = mid + 1;
+        }
+        return target <= nums[low] ? low : low + 1;
+    }
+}
