d8183eb645a521edc77b40b15180ca16c3c8957e
Hao-Ping Lin <pinglin927@gmail.com>
2020-05-18 22:37:51 -0400

    Update notes

--- JAVA/189 - Rotate Array.java
+++ JAVA/189 - Rotate Array.java
@@ -6,24 +6,67 @@
  Time: O(n)
  Space: O(1)
 
+ Approach 2 : left and right
+ First add the elements that will exceed the array after rotate to the list, 
+ then the remaining elements.
+ Time: O(2n)
+ Space: O(n)
+
 */
 
 /* Approach 1 */
 class Solution {
     public void rotate(int[] nums, int k) {
+        // Handle k value
         k %= nums.length;
         int c = 0;
+        
+        // Iterate the array
         for (int i = 0; i < nums.length; i++) {
-            int startId = i, startVal = nums[i];
+            int startId = i;
+            int startVal = nums[i];
+            
+            // Use do to loop the array
             do {
-                int newPos = (startId + k) % nums.length;
-                int tmp = nums[newPos];
-                nums[newPos] = startVal;
-                startId = newPos;
-                startVal = tmp;
+                int nextId = (startId + k) % nums.length;
+                int nextVal = nums[nextId];
+                
+                // Copy the current val to new Index
+                nums[nextId] = startVal;
+                
+                // Update the id
+                startId = nextId;
+                startVal = nextVal;
                 c++;
-            } while (startId != i);
+            } while (i  != startId);
+            // If all element were saw before iteration, return
             if (c == nums.length) return;
+        }
+    }
+}
+
+/* Approach 2 */
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        // Handle k value
+        k %= len;
+        
+        // Result list
+        List<Integer> res = new ArrayList<Integer>();
+        
+        // Handle left side element
+        for (int i = len - k; i < len; i++) {
+            res.add(nums[i]);
+        }
+        
+        // Handle right side element
+        for (int i = 0; i < len - k; i++) {
+            res.add(nums[i]);
+        }
+        
+        for (int i = 0; i < res.size(); i++) {
+            nums[i] = res.get(i);
         }
     }
 }
