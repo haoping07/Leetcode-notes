9aa9661afbc96b5ac98b5e1a24f09cb6fdf77c45
Hao-Ping Lin <pinglin927@gmail.com>
2020-05-20 11:13:42 -0400

    Update notes

--- JAVA/299 - Bulls and Cows.java
+++ JAVA/299 - Bulls and Cows.java
@@ -1,17 +1,16 @@
 /*
  299. Bulls and Cows (E)
 
- Approach 1 :
- Use array and two For-loop.
- 1. Iterates secret and guess at the same time. If two letter are the same, bull++.
-    Counts the appear of letter and stores to the array by its ASCII as key. 
- 2. Iterates two arrays at the same time, take the minumum in each and adds to the cow;
- * The amount of cow is all exist numbers (whether the position is wrong) - position is correct (bull)
- Time: O(n)
- Space: O(n)
+ Approach 1 : Use two freq array
+ Loop secret and guess array at the same time, if element match, increment bull;  
+ If not match, increment their frequency to the separate frequency array. Scan 
+ two frequency arrays at the same time and sum the minimum frequency of the two. (cow) 
+ Time: O(n + 10)
+ Space: O(2n)
 
- Approach 2 :
- Optimize approach 1 to only using one array and one pass
+ Approach 2 : Use one freq array
+ Loop secret and guess array at the same time, If element match, increment bull;
+ If not match, increment their frequency to AN frequency array and count the cow. 
  Time: O(n)
  Space: O(n)
 
@@ -24,11 +23,12 @@ class Solution {
         int[] g = new int[10];
         int bull = 0, cow = 0;
         for (int i = 0; i < secret.length(); i++) {
-            int x = secret.charAt(i) - '0';
-            int y = guess.charAt(i) - '0';
-            if (x == y) bull++;
-            s[x]++;
-            g[y]++;
+            if (secret.charAt(i) == guess.charAt(i))
+                bull++;
+            else {
+                s[secret.charAt(i) - '0']++;
+                g[guess.charAt(i) - '0']++;
+            }
         }
         
         for (int i = 0; i < 10; i++) {
@@ -36,28 +36,37 @@ class Solution {
         }
         
         StringBuilder str = new StringBuilder();
-        return str.append(bull).append("A").append(cow - bull).append("B").toString();
+        return str.append(bull).append("A").append(cow).append("B").toString();
     }
 }
 
 /* Approach 2 */
 class Solution {
     public String getHint(String secret, String guess) {
-        int[] c = new int[10];
+        int[] freq = new int[10];
         int bull = 0, cow = 0;
         for (int i = 0; i < secret.length(); i++) {
+            // If the number match, increment the bull
             if (secret.charAt(i) == guess.charAt(i))
                 bull++;
+            // If not match, count the cow
             else {
-                int d1 = secret.charAt(i) - '0';
-                if (c[d1] < 0) cow++;
-                c[d1]++;
-            
-                int d2 = guess.charAt(i) - '0';
-                if (c[d2] > 0) cow++;
-                c[d2]--;
+                int s = secret.charAt(i) - '0';
+                // If the number was showed in guess before and now is in secret,
+                // increment the cow.
+                if (freq[s] < 0) cow++;
+                // and increment its frequency
+                freq[s]++;
+                
+                int g = guess.charAt(i) - '0';
+                // If the number was showed in secret before and now is in guess,
+                // increment the cow.
+                if (freq[g] > 0) cow++;
+                // and decrement its frequency
+                freq[g]--;
             }
         }
+        
         StringBuilder str = new StringBuilder();
         return str.append(bull).append("A").append(cow).append("B").toString();
     }
