698ba1a130c744e0eaf223d3b6bfe7c53970d9e1
Hao-Ping Lin <pinglin927@gmail.com>
2020-05-21 03:18:40 -0400

    update notes

--- C++/145 - Binary Tree Postorder Traversal.cpp
+++ C++/145 - Binary Tree Postorder Traversal.cpp
@@ -1,11 +1,11 @@
 /*
  145. Binary Tree Postorder Traversal (H)
 
- Approach 1 : Recursion
+ Approach 1: Recursion
  Time: O(n)
  Space: O(n)
 
- Approach 2 : Iteration
+ Approach 2: DFS
  1. Traverse to the end of the left child.
  2. Peek parent
    2.1. If parent->right exist and havn't been visited yet, move root to parent->right and goto Step 1
@@ -15,50 +15,45 @@
 
 */
 
-/* Approach 1 : Recursion */
+// Approach 1
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        Postorder(root);
+        vector<int> res;
+        postorder(root, res);
         return res;
     }
-
-    void Postorder(TreeNode* root)
-    {
+    
+    void postorder(TreeNode* root, vector<int>& res) {
         if (!root) return;
-        Postorder(root->left);
-        Postorder(root->right);
+        postorder(root->left, res);
+        postorder(root->right, res);
         res.push_back(root->val);
     }
-
-private:
-    vector<int> res;
 };
 
-/* Approach 2 : Iteration */
+// Approach 2
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> res;
         stack<TreeNode*> todo;
         TreeNode* visited = nullptr;
-
-        while (!todo.empty() || root)
-        {
-            while (root)
-            {
+        while (!todo.empty() || root) {
+            if (root) {
                 todo.push(root);
                 root = root->left;
             }
-
-            TreeNode* peekNode = todo.top();
-            if (peekNode->right && peekNode->right != visited)
-                root = peekNode->right;
-            else
-            {
-                res.push_back(peekNode->val);
-                todo.pop();
-                visited = peekNode;
+            else {
+                TreeNode* peekNode = todo.top();
+                if (peekNode->right && peekNode->right != visited) {
+                    root = peekNode->right;
+                }
+                else {
+                    res.push_back(peekNode->val);
+                    todo.pop();
+                    visited = peekNode;
+                }
             }
         }
         return res;
