53be15b67847ad3fa48905a93c3c3920bda64669
Hao-Ping Lin <pinglin927@gmail.com>
2020-05-21 03:07:50 -0400

    Update notes

--- C++/94 - Binary Tree Inorder Traversal.cpp
+++ C++/94 - Binary Tree Inorder Traversal.cpp
@@ -1,51 +1,45 @@
 /*
  94. Binary Tree Inorder Traversal (M)
  
- Approach 1 : Recursion
+ Approach 1: Recursion
  Time: O(n)
  Space: O(n)
 
- Approach 2 : Iteration
+ Approach 2: DFS
  Time: O(n)
  Space: O(n)
 
 */
 
-/* Approach 1 : Recursion */
+// Approach 1
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        Inorder(root);
+        vector<int> res;
+        inorder(root, res);
         return res;
     }
-
-    void Inorder(TreeNode* root)
-    {
+    
+    void inorder(TreeNode* root, vector<int>& res) {
         if (!root) return;
-        Inorder(root->left);
+        inorder(root->left, res);
         res.push_back(root->val);
-        Inorder(root->right);
+        inorder(root->right, res);
     }
-
-private:
-    vector<int> res;
 };
 
-/* Approach 2 : Iteration */
+// Approach 2
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
         stack<TreeNode*> todo;
-        while (!todo.empty() || root)
-        {
-            if (root)
-            {
+        while (!todo.empty() || root) {
+            if (root) {
                 todo.push(root);
                 root = root->left;
             }
-            else
-            {
+            else {
                 root = todo.top();
                 todo.pop();
                 res.push_back(root->val);
