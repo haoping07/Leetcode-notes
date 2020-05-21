5759039bcc3782af55636fde85f3eeba9eddc79f
Hao-Ping Lin <pinglin927@gmail.com>
2020-05-21 02:41:05 -0400

    Updatye notes

--- C++/134 - Gas Station.cpp
+++ C++/134 - Gas Station.cpp
@@ -1,27 +1,34 @@
 /*
- 134. Gas Station
- Medium
+ 134. Gas Station (M)
+ 
+ Approach 1:
+ Loop two lists, if the sum gas isn't enough to the next station, try the
+ next one of the current node. Also, if the the total gas isn't enough to
+ go through all nodes, return -1;
+ Time: O(n)
+ Space: O(1) 
+ 
 */
 
+// Approach 1
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int totalGas = 0;
-        for (int i = 0; i < gas.size(); i++)
-            totalGas += gas[i] - cost[i];
-        if (totalGas < 0)
-            return -1;
-
-        int sumGas = 0, index = 0;
-        for (int i = 0; i < gas.size(); i++)
-        {
-            sumGas += gas[i] - cost[i];
-            if (sumGas < 0)
-            {
-                sumGas = 0;
+        int sum = 0;
+        int total = 0;
+        int index = 0;
+        for (int i = 0; i < gas.size(); i++) {
+            // Compute if the fuel is enough to the next station
+            sum += gas[i] - cost[i];
+            // If the fuel isn't enough
+            if (sum < 0) {
+                // Initialize the condition
+                sum = 0;
                 index = i + 1;
             }
+            // We also need to check if the fuel is enough for a circular route
+            total += gas[i] - cost[i];
         }
-        return index;
+        return total < 0 ? -1 : index;
     }
 };
