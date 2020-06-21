a548a4fd59ba6a67dd8724ee2372c81e3301ab08
Hao-Ping Lin <pinglin927@gmail.com>
2020-06-20 21:20:45 -0400

    Add approach

--- C++/112 - Path Sum.cpp
+++ C++/112 - Path Sum.cpp
@@ -1,43 +1,69 @@
 /*
- 112. Path Sum
- Easy
+ 112. Path Sum (E)
+
+ A1: Preorder(top-down)
+ Preorder the tree, if the path sum isn't fullfill the given sum, move to its 
+ child and add its value
+ Time: O(n)
+ Space: O(leaf)
+
+ A2: Preorder(top-down) recursion
+ If the current node isn't a leaf, deduct sum with the current node value and
+ move to its child 
+ Time: O(n)
+ Space: O(depth)
+ 
 */
+
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int sum) {
-        if (!root)
-            return false;
-        stack<TreeNode*> nodeStack;
-        stack<int> sumStack;
-        nodeStack.push(root);
-        sumStack.push(root->val);
-        while (!nodeStack.empty())
-        {
-            root = nodeStack.top();
-            nodeStack.pop();
-            int sumTemp = sumStack.top();
-            sumStack.pop();
- 
-            if (!root->left && !root->right)
-            {
-                if (sumTemp == sum)
-                    return true;
-                continue;
+        if (!root) return false;
+        // Queue for to-do nodes
+        queue<TreeNode*> todo; 
+        // Queue for the path sums
+        queue<int> sums;
+        
+        // Init the algo
+        todo.push(root);
+        sums.push(root->val);
+        
+        while (!todo.empty()) {
+            
+            // Get the to-do node and its path sum 
+            root = todo.front();
+            todo.pop();
+            int curSum = sums.front();
+            sums.pop();
+            
+            // If the to-do node is a leaf, check if fullfill the given sum
+            if (!root->left && !root->right) {
+                if (sum == curSum) return true;
             }
- 
-            if (root->right)
-            {
-                nodeStack.push(root->right);
-                sumStack.push(sumTemp + root->right->val);
+            
+            // If the to-do node is not a leaf, update the queue
+            if (root->left) {
+                todo.push(root->left);
+                sums.push(curSum + root->left->val);
             }
- 
-            if (root->left)
-            {
-                nodeStack.push(root->left);
-                sumStack.push(sumTemp + root->left->val);
+            if (root->right) {
+                todo.push(root->right);
+                sums.push(curSum + root->right->val);
             }
         }
- 
         return false;
+    }
+};
+
+
+// A2
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        // Use leaf to stop the recursion because we can't know the entry point
+        if (!root) return false;
+        if (!root->left && !root->right && root->val == sum) 
+            return true;
+        else return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
     }
 };
