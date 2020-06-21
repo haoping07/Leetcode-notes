da32b208c2bd55a98e2583e758753b865f92ea17
Hao-Ping Lin <pinglin927@gmail.com>
2020-06-21 16:11:30 -0400

    Update notes

--- C++/113 - Path Sum II.cpp
+++ C++/113 - Path Sum II.cpp
@@ -1,32 +1,40 @@
 /*
- 113. Path Sum II
- Medium
+ 113. Path Sum II (M)
+
+ A1: Preorder (top-down) recursion
+ Preorder the tree, use a vector to store the current path, if fullfill the
+ given sum, save the path to result
+ Time: O(n)
+ Space: O(depth)
+
 */
 
 class Solution {
 public:
     vector<vector<int>> pathSum(TreeNode* root, int sum) {
-        vector<vector<int>> result;
-        vector<int> cur;
-        DFS(root, sum, cur, result);
-        return result;
+        vector<vector<int>> res;
+        vector<int> sub;
+        
+        pathSum(root, sum, sub, res);
+        return res;
     }
- 
-    void DFS(TreeNode* root, int gap, vector<int>& cur, vector<vector<int>>& result)
-    {
-        if (root == nullptr)
-            return;
- 
-        cur.push_back(root->val);
- 
-        if (root->left == nullptr && root->right == nullptr)
-        {
-            if (gap == root->val)
-                result.push_back(cur);
+    
+    void pathSum(TreeNode* root, int sum, vector<int>& sub, vector<vector<int>>& res) {
+        if (!root) return;
+        
+        // Add the current node in to path list
+        sub.push_back(root->val);
+        
+        // Check if fullfill the given sum when it is a leaf node
+        if (!root->left && !root->right && root->val == sum) {
+            res.push_back(sub);
         }
- 
-        DFS(root->left, gap - root->val, cur, result);
-        DFS(root->right, gap - root->val, cur, result);
-        cur.pop_back();
+        
+        // If not, update the sum and move to the next level
+        if (root->left) pathSum(root->left, sum - root->val, sub, res);
+        if (root->right) pathSum(root->right, sum - root->val, sub, res);
+        
+        // Update the path for the next node
+        sub.pop_back();
     }
 };
