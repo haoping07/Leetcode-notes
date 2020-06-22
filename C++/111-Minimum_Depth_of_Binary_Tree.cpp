239af424ecdbd63638fab9f10734e6af0d044ac6
Hao-Ping Lin <pinglin927@gmail.com>
2020-06-22 15:22:15 -0400

    Add recursion approach

--- C++/111-Minimum_Depth_of_Binary_Tree.cpp
+++ C++/111-Minimum_Depth_of_Binary_Tree.cpp
@@ -1,16 +1,50 @@
 /*
- 111. Minimum Depth of Binary Tree
- Easy
+ 111. Minimum Depth of Binary Tree (E)
+ 
+ A1: DFS (Btm-top)
+ Iterate to the leaf and return the smaller increment depth one level by level.
+ Beware of returning the "0" depth left or right child.
+ We don't use top-btm approach here because it will need to init the depth to 
+ INIT_MAX first which complicates the problem.
+ Time: O(n)
+ Space: O(n)
+
+ A2: Preoder (level by level)
+ Time: O(n)
+ Space: O(n)
+
 */
 
+// A1
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if (!root)
-            return 0;
+        if (!root) return 0;
+        if (!root->left && !root->right) return 1;
+        
+        // Get the left and right child minimum depth
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+        
+        // If one of it is zero, avoid returning it
+        if (left == 0 || right == 0) 
+            return left + right + 1;
+        
+        // Otherwise, return the smaller one
+        return min(left, right) + 1;
+    }
+};
+
+
+// A2
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root) return 0;
         int count = 0;
         queue<TreeNode*> todo;
         todo.push(root);
+
         while (!todo.empty())
         {
             int size = todo.size();
@@ -19,12 +53,9 @@ public:
             {
                 root = todo.front();
                 todo.pop();
-                if (!root->left && !root->right)
-                    return count;
-                if (root->left)
-                    todo.push(root->left);
-                if (root->right)
-                    todo.push(root->right);
+                if (!root->left && !root->right) return count;
+                if (root->left) todo.push(root->left);
+                if (root->right) todo.push(root->right);
             }
         }
         return -1;
