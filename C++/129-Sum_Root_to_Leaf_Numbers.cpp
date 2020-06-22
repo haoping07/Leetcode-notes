8ca1964a8c8f37c0b6acc2f01ddbe3f659d76cd1
Hao-Ping Lin <pinglin927@gmail.com>
2020-06-22 04:21:36 -0400

    Update notes

--- C++/129-Sum_Root_to_Leaf_Numbers.cpp
+++ C++/129-Sum_Root_to_Leaf_Numbers.cpp
@@ -1,29 +1,31 @@
 /*
- 129. Sum Root to Leaf Numbers
- Medium
+ 129. Sum Root to Leaf Numbers (M)
+
+ A1: DFS
+ DFS the tree and save the node value, if the node is a leaf, add it to the total sum.
+ Time: O(n)
+ Space: O(n)
+
 */
 
+// A1
 class Solution {
 public:
     int sumNumbers(TreeNode* root) {
-        if (!root)
-            return 0;
-        sum = 0;
-        DFS(root, 0);
+        int sum = 0;
+        DFS(root, sum, 0);
         return sum;
     }
- 
-    void DFS(TreeNode* root, int currentSum)
-    {
-        currentSum = currentSum * 10 + root->val;
-        if (!root->left && !root->right)
-            sum += currentSum;
-        if (root->left)
-            DFS(root->left, currentSum);
-        if (root->right)
-            DFS(root->right, currentSum);
+    
+    void DFS(TreeNode* root, int& sum, int curSum) {
+        if (!root) return;
+        curSum = curSum * 10 + root->val;
+
+        // If the node is a leaf, add the current sum to the total sum
+        if (!root->left && !root->right) sum += curSum;
+
+        // If not a leaf, move into it
+        if (root->left) DFS(root->left, sum, curSum);
+        if (root->right) DFS(root->right, sum, curSum);
     }
- 
-private:
-    int sum;
 };
