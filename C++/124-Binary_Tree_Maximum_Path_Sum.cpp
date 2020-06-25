14787564deb99b5f20b98de069569e3dff86b46c
Hao-Ping Lin <pinglin927@gmail.com>
2020-06-25 00:48:29 -0400

    Update notes

--- C++/124-Binary_Tree_Maximum_Path_Sum.cpp
+++ C++/124-Binary_Tree_Maximum_Path_Sum.cpp
@@ -1,31 +1,35 @@
 /*
  124. Binary Tree Maximum Path Sum (H)
 
- Notes:
- Approach 1 : Divide-and-conquer (Postorder)
- Solution ref from HuaHua(https://youtu.be/9ZNky1wqNUw)
- Check left and right child and get max.
+ A1: Divide-and-conquer (DFS btm-up)
+ Ref: HuaHua(https://youtu.be/9ZNky1wqNUw)
  Time: O(n)
  Space: O(n)
  
 */
 
-// Approach 1 : Divide-and-conquer
+// A1
 class Solution {
 public:
-	int maxPathSum(TreeNode* root) {
-		int res = INT_MIN;
-		maxPathSum(root, res);
-		return res;
-	}
-
-	int maxPathSum(TreeNode* root, int& res)
-	{
-		if (!root) return 0;
-		int left = max(0, maxPathSum(root->left, res));
-		int right = max(0, maxPathSum(root->right, res));
-		int sum = left + right + root->val;
-		res = max(res, sum);
-		return max(left, right) + root->val;
-	}
+    int maxPathSum(TreeNode* root) {
+        int currMax = INT_MIN;
+        maxPathSum(root, currMax);
+        return currMax;
+    }
+    
+    int maxPathSum(TreeNode* root, int& currMax) {
+        // 如果root是空的返回0
+        if (!root) return 0;
+        
+        // 取得左最大及又最大
+        int left = max(0, maxPathSum(root->left, currMax));
+        int right = max(0, maxPathSum(root->right, currMax));
+        
+        // 假設在此轉折, 左右加自己與歷史最大比較
+        int turn = left + right + root->val;
+        currMax = max(currMax, turn);
+        
+        // 假設不在此轉折, 左右取大的加自己往上傳
+        return max(left, right) + root->val;
+    }
 };
