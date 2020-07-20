5562f39a1fb1b1dd48a1623083ef25999fb266a5
Hao-Ping Lin <pinglin927@gmail.com>
2020-07-20 18:21:33 +0800

    update notes

--- C++/112-Path_Sum.cpp
+++ C++/112-Path_Sum.cpp
@@ -1,47 +1,40 @@
 /*
  112. Path Sum (E)
 
- A1: Preorder(top-down)
+ Preorder(top-down)
  Preorder the tree, if the path sum isn't fullfill the given sum, move to its 
  child and add its value
- Time: O(n)
- Space: O(leaf)
+ T:O(n)
+ S:O(leaf)
 
- A2: Preorder(top-down) recursion
+ Preorder(top-down) recursion
  If the current node isn't a leaf, deduct sum with the current node value and
  move to its child 
- Time: O(n)
- Space: O(depth)
+ T:O(n)
+ S:O(depth)
  
 */
 
+// Preorder(top-down)
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int sum) {
         if (!root) return false;
-        // Queue for to-do nodes
+        // Queue for todo nodes and current sum
         queue<TreeNode*> todo; 
-        // Queue for the path sums
         queue<int> sums;
-        
-        // Init the algo
         todo.push(root);
         sums.push(root->val);
-        
-        while (!todo.empty()) {
-            
-            // Get the to-do node and its path sum 
+
+        while (!todo.empty()) { 
             root = todo.front();
             todo.pop();
             int curSum = sums.front();
             sums.pop();
             
-            // If the to-do node is a leaf, check if fullfill the given sum
             if (!root->left && !root->right && curSum == sum) {
                 return true;
             }
-            
-            // If the to-do node is not a leaf, update the queue
             if (root->left) {
                 todo.push(root->left);
                 sums.push(curSum + root->left->val);
@@ -56,7 +49,7 @@ public:
 };
 
 
-// A2
+// Preorder(top-down) recursion
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int sum) {
